4913e351c2c13fea4a4cde694b0518fcf19e71f5
Gihan Siriwardhana <gihan@onramper.com>
2023-07-10 09:16:59 +0530

    import fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '@/styles/Home.module.scss';
-import Hero from '@/components/Hero/Hero';
-import About from '@/components/About/About';
-import Experience from '@/components/Experience/Experience';
-import Education from '@/components/Education/Education';
-import Volunteer from '@/components/Volunteer/Volunteer';
-import GalleryComp from '@/components/Gallery/Gallery';
+import styles from '../styles/Home.module.scss';
+import Hero from '../components/Hero/Hero';
+import About from '../components/About/About';
+import Experience from '../components/Experience/Experience';
+import Education from '../components/Education/Education';
+import Volunteer from '../components/Volunteer/Volunteer';
+import GalleryComp from '../components/Gallery/Gallery';
 
 export default function Home() {
   return (
